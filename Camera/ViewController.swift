159fea3675abcbc25f7c60edc2f0e174d63a6b00
egapool <egpersent@gmail.com>
2017-05-06 23:23:38 +0900

    AVCapturePhotoOutputでカメラ画像をUIImageviewに描画

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -8,20 +8,76 @@
 
 import Alamofire
 import UIKit
+import AVFoundation
 
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+    var captureSesssion: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    @IBAction func takeIt2(_ sender: Any) {
+    
+        // フラッシュとかカメラの細かな設定
+        let settingsForMonitoring = AVCapturePhotoSettings()
+        settingsForMonitoring.flashMode = .auto
+        settingsForMonitoring.isAutoStillImageStabilizationEnabled = true
+        settingsForMonitoring.isHighResolutionPhotoEnabled = false
+        // シャッターを切る
+        stillImageOutput?.capturePhoto(with: settingsForMonitoring, delegate: self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         
-        let camera = UIImagePickerControllerSourceType.savedPhotosAlbum
-        if UIImagePickerController.isSourceTypeAvailable(camera) {
-            let picker = UIImagePickerController()
-            picker.sourceType = camera
-            picker.delegate = self
-            self.present(picker, animated:true)
+        captureSesssion = AVCaptureSession()
+        stillImageOutput = AVCapturePhotoOutput()
+        
+        captureSesssion.sessionPreset = AVCaptureSessionPreset1920x1080 // 解像度の設定
+        
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            
+            // 入力
+            if (captureSesssion.canAddInput(input)) {
+                captureSesssion.addInput(input)
+                
+                // 出力
+                if (captureSesssion.canAddOutput(stillImageOutput)) {
+                    captureSesssion.addOutput(stillImageOutput)
+                    captureSesssion.startRunning() // カメラ起動
+                    
+                    previewLayer = AVCaptureVideoPreviewLayer(session: captureSesssion)
+                    previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect // アスペクトフィット
+                    previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait // カメラの向き
+                    
+                    imageView.layer.addSublayer(previewLayer!)
+                    
+                    // ビューのサイズの調整
+                    previewLayer?.position = CGPoint(x: self.imageView.frame.width / 2, y: self.imageView.frame.height / 2)
+                    previewLayer?.bounds = imageView.frame
+                }
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    // デリゲート。カメラで撮影が完了した後呼ばれる。JPEG形式でフォトライブラリに保存。
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if let photoSampleBuffer = photoSampleBuffer {
+            // JPEG形式で画像データを取得
+            let photoData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+            let image = UIImage(data: photoData!)
+            
+            // フォトライブラリに保存
+            UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
         }
     }
     
@@ -33,7 +89,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.uploadFile(image)
         
         self.imageView.image = image
-//        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        //        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
         
     }
     
@@ -41,12 +97,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func uploadFile(_ cameraImage: UIImage) {
         let imageData = UIImageJPEGRepresentation(cameraImage, 0.9)
         
@@ -54,7 +110,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         Alamofire.upload(
             multipartFormData: { multipartFormData in
                 multipartFormData.append(imageData!, withName: "camera_img",fileName: "camera_img", mimeType: "image/jpeg")
-            },
+        },
             to: "http://192.168.0.107/",
             encodingCompletion: { encodingResult in
                 switch encodingResult {
@@ -70,12 +126,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 case .failure(let encodingError):
                     print(encodingError)
                 }
-            }
+        }
         )
         
         
-
+        
     }
-
+    
 }
 
